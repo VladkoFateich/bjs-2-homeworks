610bd158e00b6da75bd6d894339cc1d044284194
Vladislav Fateev <Fateev_wbd@mail.ru>
2022-05-10 16:48:59 +0500

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,14 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  if (typeof a !== "number") return [];
+  if (typeof b !== "number") return [];
+  if (typeof c !== "number") return [];
+  const d = b ** 2 - 4 * a * c;
+  const xOne = -b / (2 * a);
+  const xTwo = (-b + Math.sqrt(d)) / (2 * a);
+  const xThree = (-b - Math.sqrt(d)) / (2 * a);
+  if (d < 0) return [];
+  if (d === 0) return [xOne];
+  return [xTwo, xThree];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
