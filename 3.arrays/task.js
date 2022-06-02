113f116b0cf16664b2fe209a35c57b854016925a
Vladko Fateich <Fateev_wbd@mail.ru>
2022-06-02 11:53:04 +0500

    function

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,25 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+for(let i = 0; i < arr1.length; i++) {
+  if(arr1[i] === arr2[i] && arr1.length === arr2.length) {
+  result = true
+} else {
+  result = false
+} 
+}
 
   return result; // boolean
 }
+console.log(compareArrays([1, 2], [1, 2, 3]))
 
 function advancedFilter(arr) {
   let resultArr;
+  resultArr = arr
+    .filter((elem) => elem > 0 && elem % 3 === 0)
+    .map((newElem) => {
+      return newElem * 10;
+    });
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr;
 }
+console.log(advancedFilter([-10,-21,12,123]))
