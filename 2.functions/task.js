2215d2ddadd4a63848e91cc726e592b58466259d
VladkoFateich <92536769+VladkoFateich@users.noreply.github.com>
2022-07-06 19:25:31 +0500

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,47 +1,59 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min = arr [0], max = arr[0], sum = 0, avg;
-  let i = 0;
-  while (i < arr.length) {
-    if (arr[i] > max) max = arr[i];
-    if (arr[i] < min) min = arr[i];
+  let min, max, sum, avg;
+
+  max = - Infinity;
+  min = Infinity;
+  sum = 0
+  avg = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) max = arr[i];
+    if (min > arr[i]) min = arr[i];
     sum += arr[i];
-    i++;
   }
-  avg = (sum / arr.length);
-  avg = Number(avg.toFixed(2));
+  avg = Number((sum / arr.length).toFixed(2));
+
+
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum = 0;
-  arr.forEach(element => {
-    sum += element;
-  });
-  // Ваш код
+  let sum;
+sum =0
+  for (let i = 0; i < arr.length; i++) sum += arr[i];
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max = -Infinity;
-  arrOfArr.forEach(element => {
-    if (func(element) > max) max = func(element);
-  });
+  let max;
+
+
   // Ваш кода
   // for ...
+
+
+  max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const currentArrSum = func(arrOfArr[i]);
+    if (max < currentArrSum) max = currentArrSum;
+  }
   
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let min = arr[0], max = arr[0];
-  arr.forEach(element => {
-    if (element > max) max = element;
-    if (element < min) min = element;
-  });
-  return Math.abs (max - min)
-  // Ваш код
+  let min, max, dif;
+  max = -Infinity;
+  min = Infinity;
+  dif = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) max = arr[i];
+    if (min > arr[i]) min = arr[i];
+  }
+  dif = Math.abs(max - min);
+  return dif;
 }
