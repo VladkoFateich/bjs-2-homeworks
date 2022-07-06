b286ead6eb9856f4492266ad24b5497bc0d8c8ec
VladkoFateich <92536769+VladkoFateich@users.noreply.github.com>
2022-07-06 19:08:40 +0500

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,64 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-<<<<<<< HEAD
-  min = Infinity;
-  min = Infinity;
-  arr = [];
-  for (i = -100; i <= 100; i++) arr.push(i);
-=======
-
-  max = -Infinity;
-  min = Infinity;
-  sum = 0
-  for (var i = 0; i < arr.length; i++) {
-    if (max < arr[i]) max = arr[i];
-    if (min > arr[i]) min = arr[i];
+  let min = arr [0], max = arr[0], sum = 0, avg;
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
     sum += arr[i];
+    i++;
   }
-  avg = Number((sum / arr.length).toFixed(2));
->>>>>>> 2b485fe8271126d3f1164cb9c7cca372d3832347
-
+  avg = (sum / arr.length);
+  avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-sum =0
-  for (let i = 0; i < arr.length; i++) sum += arr[i];
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+  // Ваш код
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-<<<<<<< HEAD
+  let max = -Infinity;
+  arrOfArr.forEach(element => {
+    if (func(element) > max) max = func(element);
+  });
   // Ваш кода
   // for ...
-
-=======
-  max = -Infinity;
-  for (let i = 0; i < arrOfArr.length; i++) {
-    const currentArrSum = func(arrOfArr[i]);
-    if (max < currentArrSum) max = currentArrSum;
-  }
   
->>>>>>> 2b485fe8271126d3f1164cb9c7cca372d3832347
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let min, max, dif;
-  max = -Infinity;
-  min = Infinity;
-  dif = 0
-  for (let i = 0; i < arr.length; i++) {
-    if (max < arr[i]) max = arr[i];
-    if (min > arr[i]) min = arr[i];
-  }
-  dif = Math.abs(max - min);
-  return dif;
+  let min = arr[0], max = arr[0];
+  arr.forEach(element => {
+    if (element > max) max = element;
+    if (element < min) min = element;
+  });
+  return Math.abs (max - min)
+  // Ваш код
 }
