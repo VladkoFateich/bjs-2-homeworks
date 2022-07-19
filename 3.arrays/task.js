e76868331526abf14187f7382151f51d274cfbfb
VladkoFateich <92536769+VladkoFateich@users.noreply.github.com>
2022-07-19 12:58:10 +0500

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,47 +1,27 @@
 function compareArrays(arr1, arr2) {
+
   let result;
 
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] === arr2[i] && arr1.length === arr2.length) {
-    result = true
-  } else {
-    result = false
-  } 
-  }
-  return result; // boolean
-}
-compareArrays([8, 1, 2], [8, 1, 2])
+  return arr1.length == arr2.length && arr1.every((v,i)=>v === arr2[i])
+
+};
+
+
+
 
 function advancedFilter(arr) {
+
   let resultArr;
-arr.forEach(element => {
-  
-});
 
-  return resultArr; // array
-}
-advancedFilter([-10,-21,12,123])
-for(let i = 0; i < arr1.length; i++) {
-  if(arr1[i] === arr2[i] && arr1.length === arr2.length) {
-  result = true
-} else {
-  result = false
-} 
-}
 
-  return result; // boolean
-}
-console.log(compareArrays([1, 2], [1, 2, 3]))
 
-function advancedFilter(arr) {
-  let resultArr;
+
   resultArr = arr
-    .filter((elem) => elem > 0 && elem % 3 === 0)
-    .map((newElem) => {
-      return newElem * 10;
-    });
 
-  return resultArr;
+  .filter((arr) => arr % 3 === 0 && arr > 0) .map((arr) => arr * 10);
+
+  console.log(resultArr);
+
+  return resultArr; 
+
 }
-console.log(advancedFilter([-10,-21,12,123]))
-
